57f929ec455ce3ae1d5ae17940935418dbadbc20
pongopof <shandopandoo@gmail.com>
2023-09-30 19:48:53 +0200

    radquiz after modification ResultScreen and adding FullScreenImage as a possibiloity to make image bigger

--- app/src/main/java/com/example/radimagequiz/ui/layout/ResultScreen.kt
+++ app/src/main/java/com/example/radimagequiz/ui/layout/ResultScreen.kt
@@ -1,20 +1,16 @@
 package com.example.radimagequiz.ui.layout
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.lifecycle.ViewModel
 import com.example.data.Answer
 import com.example.data.CorrectAnswer
 import com.example.data.Question
